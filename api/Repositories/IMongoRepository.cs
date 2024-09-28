514bd0b324c3c8f56553732c14909a4c8a385782
it21058578 <it21058578@my.sliit.lk>
2024-09-28 13:10:50 +0530

    Added EFCore integration

--- api/Repositories/IMongoRepository.cs
+++ api/Repositories/IMongoRepository.cs
@@ -2,15 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace api.Repositories
 {
     public interface IMongoRepository<T> where T : class
     {
-        T GetById(string id);
+        T? GetById(string id);
+        T? GetById(ObjectId id);
         IEnumerable<T> GetAll();
         void Add(T entity);
+        void AddMany(IEnumerable<T> entities);
         void Update(T entity);
+        void UpdateMany(IEnumerable<T> entities);
         void Delete(T entity);
+        void DeleteMany(IEnumerable<T> entities);
     }
 }
---
+++ api/Services/AppDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace api.Services
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Order> Orders { get; init; }
+        public DbSet<Product> Products { get; init; }
+        public DbSet<User> Users { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Order>().HasKey(o => o.Id);
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+        }
+    }
+}
