b8775f944df3f336dab77ed1c3b42a6f981b425a
Tharindu Gunasekera <gunasekeratharindu@gmail.com>
2024-10-07 23:33:11 +0530

    Removed url validation

--- api/DTOs/Requests/CreateProductRequestDto.cs
+++ api/DTOs/Requests/CreateProductRequestDto.cs
@@ -32,7 +32,6 @@ namespace api.DTOs.Requests
         [Required]
         [Range(0, int.MaxValue)]
         public int LowStockThreshold { get; set; } = 10;
-        [Url]
         [Required]
         public string ImageUrl { get; set; } = "";
     }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9cf80d9bd41389017a5026fdb41675e2dc074b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee4b98a2e56d56d7bda5608352bc955745953f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
