df89750c0db388a9d96c24570f0b55f7eebbf224
Dinuka Dissanayake <it21024818@my.sliit.lk>
2024-10-07 23:17:11 +0530

    vendors page added

--- web/src/views/PageNotFound.tsx
+++ web/src/views/PageNotFound.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from './includes/Header';
 import Footer from './includes/Footer';
 import NotFound from '../components/404';
+import AdminFooter from '../components/Admin/AdminFooter';
+import AdminHeader from '../components/Admin/AdminHeader';
 
 const PageNotFound = () => {
 
   return (
     <>
-        <Header />
+        <AdminHeader />
         <NotFound />
-        <Footer />
+        <AdminFooter />
     </>
 )
 
