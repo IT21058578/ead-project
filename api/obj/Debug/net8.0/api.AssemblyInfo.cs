eefe83cb30978430c2002d3ffad5278d7758b30b
Sansikx Dev <86715839+it21028014@users.noreply.github.com>
2024-10-11 23:16:03 +0530

    Done Notification fixes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0670a27f38ce50feeb81360799e26ebb08431520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212494bba3e08340a910563412b082f4522720a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
